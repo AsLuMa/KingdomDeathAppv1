653c4dd0b18760cedcd3fb25c523a5a8f613d25d
ALM <ase.mattson@gmail.com>
2021-03-21 17:48:56 +0100

    Armor and main as two activities

--- app/src/main/java/com/example/kingdomdeath/Armor.java
+++ app/src/main/java/com/example/kingdomdeath/Armor.java
@@ -1,5 +1,8 @@
 package com.example.kingdomdeath;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +14,20 @@ public class Armor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_armor);
+
+        ImageButton startMain = findViewById(R.id.startMain);
+
+
+        startMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startMain = new Intent(Armor.this, MainActivity.class);
+                startActivity(startMain);
+            }
+        });
+
+
+
 
 
 
