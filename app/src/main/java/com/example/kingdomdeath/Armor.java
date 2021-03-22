0d8a260fb0f0e8037b9434eac3987de4d66da1b8
ALM <ase.mattson@gmail.com>
2021-03-23 00:45:34 +0100

    Head save state done - working my way through the rest

--- app/src/main/java/com/example/kingdomdeath/Armor.java
+++ app/src/main/java/com/example/kingdomdeath/Armor.java
@@ -20,33 +20,94 @@ public class Armor extends AppCompatActivity {
     public static final String armorpref = "armorpref";
 
     //keys to find associated value (savestate)
-    public static final String headValue = "headValue";
-    public static final String head = "headValue";
+    public static final String headKey = "headKey";
+    public static final String headHeavy = "headValue";
 
     public static final String armsKey = "armsKey";
     public static final String armsLight = "armsLight";
     public static final String armsHeavy = "armsHeavy";
 
     public static final String bodyKey = "bodyKey";
-    public static final String waistKey = "waistKey";
-    public static final String legsKey = "legsKey";
+    public static final String bodyLight = "bodyLight";
+    public static final String bodyHeavy = "bodyHeavy";
 
+    public static final String waistKey = "waistKey";
+    public static final String waistLight = "waitsLight";
+    public static final String waistHeavy = "waistHeavy";
+
+    public static final String legsKey = "legsKey";
+    public static final String legsLight = "legsLight";
+    public static final String legsHeavy = "legsHeavy";
+
+    //tried to make Armor-object to put into SharedPreferences, but need to download gson-something to do that
+    //refactor to database storage/sqlite
+
+    //head
+    private EditText headEditText;
+    private String headValue;
+    private CheckBox headHeavyCB;
+    private boolean headHeavyBoolean;
+
+    //arms
     private EditText armsEditText;
     private String armsValue;
-
     private CheckBox armsLightCB;
     private boolean armsLightBoolean;
-
     private CheckBox armsHeavyCB;
     private boolean armsHeavyBoolean;
 
+    //body
+    private EditText bodyEditText;
+    private String bodyValue;
+    private CheckBox bodyLightCB;
+    private boolean bodyLightBoolean;
+    private CheckBox bodyHeavyCB;
+    private boolean bodyHeavyBoolean;
+
+
+    //waist
+    private EditText waistEditText;
+    private String waistValue;
+    private CheckBox waistLightCB;
+    private boolean waistLightBoolean;
+    private CheckBox waistHeavyCB;
+    private boolean waistHeavyBoolean;
+
+
+    //legs
+    private EditText legsEditText;
+    private String legsValue;
+    private CheckBox legsLightCB;
+    private boolean legsLightBoolean;
+    private CheckBox legsHeavyCB;
+    private boolean legsHeavyBoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_armor);
 
+        headEditText = (EditText) findViewById(R.id.headValue);
+        headHeavyCB = (CheckBox) findViewById(R.id.headHeavyID);
+
         armsEditText = (EditText) findViewById(R.id.armsValue);
+        armsLightCB = (CheckBox) findViewById(R.id.armsLightID);
+        armsHeavyCB = (CheckBox) findViewById(R.id.armsHeavyID);
+
+        //body
+        bodyEditText = (EditText) findViewById(R.id.bodyValue);
+        bodyLightCB = (CheckBox) findViewById(R.id.bodyLightID);
+        bodyHeavyCB = (CheckBox) findViewById(R.id.bodyHeavyID);
+
+        //waist
+        waistEditText = (EditText) findViewById(R.id.waistValue);
+        waistLightCB = (CheckBox) findViewById(R.id.waistLightID);
+        waistHeavyCB = (CheckBox) findViewById(R.id.waistHeavyID);
+
+        //legs
+        legsEditText = (EditText) findViewById(R.id.legsValue);
+        legsLightCB = (CheckBox) findViewById(R.id.legsLightID);
+        legsHeavyCB = (CheckBox) findViewById(R.id.legsHeavyID);
 
         ImageButton startMain = findViewById(R.id.startMain);
 
@@ -56,9 +117,6 @@ public class Armor extends AppCompatActivity {
         //savestate
         sharedPreferences = getSharedPreferences(armorpref,
                 Context.MODE_PRIVATE);
-
-        armsLightCB = (CheckBox) findViewById(R.id.armsLightID);
-        armsHeavyCB = (CheckBox) findViewById(R.id.armsHeavyID);
 
         load();
 
@@ -82,26 +140,107 @@ public class Armor extends AppCompatActivity {
         resetButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                headValue = "" + 0;
+                headEditText.setText(headValue);
+
                 armsValue = "" + 0;
                 armsEditText.setText(armsValue);
+
+                bodyValue = "" + 0;
+                bodyEditText.setText(bodyValue);
+
+                waistValue = "" + 0;
+                waistEditText.setText(waistValue);
+
+                legsValue = "" + 0;
+                legsEditText.setText(armsValue);
+
+                if(headHeavyCB.isChecked()){
+                    headHeavyCB.setChecked(false);
+                }
+
+                if(armsLightCB.isChecked()){
+                    armsLightCB.setChecked(false);
+                }
+
+                if(armsHeavyCB.isChecked()){
+                    armsHeavyCB.setChecked(false);
+                }
+
+                if(bodyLightCB.isChecked()){
+                    bodyLightCB.setChecked(false);
+                }
+
+                if(bodyHeavyCB.isChecked()){
+                    bodyHeavyCB.setChecked(false);
+                }
+
+                if(waistLightCB.isChecked()){
+                    waistLightCB.setChecked(false);
+                }
+
+                if(waistHeavyCB.isChecked()){
+                    waistHeavyCB.setChecked(false);
+                }
+
+                if(armsLightCB.isChecked()){
+                    armsLightCB.setChecked(false);
+                }
+
+                if(armsHeavyCB.isChecked()){
+                    armsHeavyCB.setChecked(false);
+                }
+
+
+
             }
         });
 
     }
 
     public void armsLightCheckbox(View view){
-        armsLightBoolean = armsLightCB.isChecked();
+        if(armsLightCB.isChecked()){
+            armsLightCB.setChecked(true);
+            armsLightBoolean = armsLightCB.isChecked();
+        }
+
+        else if(!armsLightCB.isChecked()){
+            armsLightCB.setChecked(false);
+            armsLightBoolean = armsLightCB.isChecked();
+        }
+
     }
 
     public void armsHeavyCheckbox(View view){
-        armsHeavyBoolean = armsHeavyCB.isChecked();
+        if(armsHeavyCB.isChecked()){
+            armsHeavyCB.setChecked(true);
+            armsHeavyBoolean = armsHeavyCB.isChecked();
+        }
+
+        else if(!armsHeavyCB.isChecked()){
+            armsHeavyCB.setChecked(false);
+            armsHeavyBoolean = armsHeavyCB.isChecked();
+        }
     }
 
+    public void headHeavyCheckbox(View view){
+        if(headHeavyCB.isChecked()){
+            headHeavyCB.setChecked(true);
+            headHeavyBoolean = headHeavyCB.isChecked();
+        }
+
+        else if(!headHeavyCB.isChecked()){
+            headHeavyCB.setChecked(false);
+            headHeavyBoolean = headHeavyCB.isChecked();
+        }
+
+    }
 
 
     public void save(){
 
         armsValue = armsEditText.getText().toString();
+        headValue = headEditText.getText().toString();
 
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
@@ -109,9 +248,16 @@ public class Armor extends AppCompatActivity {
             editor.putInt(armsKey, Integer.parseInt(armsValue));
         }
 
+        if (!"".equals(headValue)){
+            editor.putInt(headKey, Integer.parseInt(headValue));
+        }
+
         editor.putBoolean(armsLight, armsLightBoolean);
         editor.putBoolean(armsHeavy, armsHeavyBoolean);
+        editor.putBoolean(headHeavy, headHeavyBoolean);
         editor.commit();
+
+
 
     }
 
@@ -122,10 +268,15 @@ public class Armor extends AppCompatActivity {
             armsEditText.setText("" + sharedPreferences.getInt(armsKey, -1));
         }
 
+        if (sharedPreferences.contains(headKey)) {
+            headEditText.setText("" + sharedPreferences.getInt(headKey, -1));
+        }
+
+
         armsLightCB.setChecked(sharedPreferences.getBoolean(armsLight, armsLightBoolean));
         armsHeavyCB.setChecked(sharedPreferences.getBoolean(armsHeavy, armsHeavyBoolean));
 
-
+        headHeavyCB.setChecked(sharedPreferences.getBoolean(headHeavy, headHeavyBoolean));
 
     }
 }
